b13201aae03145204a07d1be27195e45026856bf
Michael Mahaffey <mayhaffs@gmail.com>
2020-05-20 06:20:11 -0500

    Update LandingPage

--- src/App.js
+++ src/App.js
@@ -7,48 +7,100 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
+import fighter from "./images/_fighter.png";
+import sorcerer from "./images/_sorcerer.png";
 
 function Navigation() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="">skills-tab</Navbar.Brand>
+      <Nav.Link href="" className="navbarBrand">
+        Skills-Tab
+      </Nav.Link>
       <Nav className="mr-auto">
-        <Nav.Link href="">Home</Nav.Link>
+        {/* <Nav.Link href="">Home</Nav.Link>
         <Nav.Link href="">Features</Nav.Link>
-        <Nav.Link href="">Pricing</Nav.Link>
+        <Nav.Link href="">Pricing</Nav.Link> */}
       </Nav>
       <Nav className="ml-auto">
         <Nav.Link href="">Login</Nav.Link>
-        <Nav.Link href="">Register</Nav.Link>
+        <Nav.Link href="">Sign Up</Nav.Link>
       </Nav>
     </Navbar>
   );
 }
 
+function Footer() {
+  return (
+    <Navbar
+      bg="dark"
+      variant="dark"
+      sticky="bottom"
+      className="justify-content-center"
+    >
+      <Nav className="justify-content-center">
+        <Nav.Link href="">Contact</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
+
+// function ImageProfile() {
+//   return ;
+//   return ;
+// }
+
 function LandingPage() {
   return (
     <Container>
       <Row>
-        <Col></Col>
-        <Col xs={8}>
+        <Col xs={0}></Col>
+        <Col xs={12}>
           <Jumbotron className="jumbotron">
-            <h1>Hello, Azeroth!</h1>
+            <h1>Skills-Tab</h1>
             <br></br>
             <p>Be the hero you've always wanted to be.</p>
-            <p>Grow your skills with skills-tab.</p>
-            <br></br>
-            <p>
-              <Button variant="primary">Learn more</Button>
-            </p>
+            <p>Grow your skills with Skills-Tab.</p>
           </Jumbotron>
         </Col>
-        <Col></Col>
+        <Col xs={0}></Col>
       </Row>
       <Row>
-        <Col></Col>
-        <Col xs={8}></Col>
-        <Col></Col>
+        <Col xs={5}>
+          <img
+            src={sorcerer}
+            alt="sorcerer"
+            className="landing-images center"
+          />
+        </Col>
+        <Col xs={2}></Col>
+        <Col xs={5}>
+          <img src={fighter} alt="fighter" className="landing-images center" />
+        </Col>
       </Row>
+      <Row>
+        <Col xs={3}></Col>
+        <Col xs={6}>
+          <p className="center landing-button">
+            <Button variant="warning" size="lg">
+              Sign Up
+            </Button>
+          </p>
+        </Col>
+        <Col xs={3}></Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col xs={3}></Col>
+        <Col xs={6}>
+          <p className="center landing-button">
+            <Button variant="warning" size="lg">
+              Login
+            </Button>
+          </p>
+        </Col>
+        <Col xs={3}></Col>
+      </Row>
+      <br></br>
     </Container>
   );
 }
@@ -58,6 +110,7 @@ function App() {
     <div className="App">
       <Navigation />
       <LandingPage />
+      <Footer />
     </div>
   );
 }
