b812c843721df3f5b4daa6d6a7442e0d27f33eb5
Michael Mahaffey <mayhaffs@gmail.com>
2020-05-30 10:53:14 -0500

    Add state to signup form

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -6,39 +6,89 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 
 export default class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { fname, lname, email, password } = this.state;
+
+    // axios example
+    // axios.post("/", { fname, lname, email, password }).then((result) => {});
+
+    // localStorage TODO
+    // localStorage.setItem("user", "");
+  };
+
   render() {
+    const { fname, lname, email, password } = this.state;
     return (
       <Container>
         <Row>
           <Col xs={1}></Col>
           <Col xs={10}>
             <br></br>
-            <Form className="signupPage">
+            <Form className="signupPage" onSubmit={this.handleSubmit}>
               <h1 className="center">Sign Up</h1>
               <hr></hr>
               <br></br>
 
               <Form.Group controlId="formBasicFirstName">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="First name" />
+                <Form.Control
+                  type="text"
+                  placeholder="First name"
+                  name="fname"
+                  value={fname}
+                  onChange={this.onChange}
+                />
                 <Form.Text className="text-muted"></Form.Text>
               </Form.Group>
 
               <Form.Group controlId="formBasicLastName">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Last name" />
+                <Form.Control
+                  type="text"
+                  placeholder="Last name"
+                  name="lname"
+                  value={lname}
+                  onChange={this.onChange}
+                />
                 <Form.Text className="text-muted"></Form.Text>
               </Form.Group>
 
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  value={email}
+                  onChange={this.onChange}
+                />
                 <Form.Text className="text-muted"></Form.Text>
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={this.onChange}
+                />
               </Form.Group>
 
               <Button variant="primary" className="btn-block" type="submit">
